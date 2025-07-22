de48fb92d98b46ef5f744e1d22a7987c5631cc7c
anwar789456 <anouarsalhi123@gmail.com>
2025-07-22 15:16:19 +0200

    fixed bug

--- routes/devisCompteurRouter.js
+++ routes/devisCompteurRouter.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const { getCounter, incrementCounter, updateCounter } = require('../controllers/devisCompteurController');
