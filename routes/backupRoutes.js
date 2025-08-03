8a6c1d7346501b0cdcaaed3bc526efd42b3360ee
anwar789456 <anouarsalhi321@gmail.com>
2025-08-03 14:48:40 +0200

    fixed api endpoint

--- routes/backupRoutes.js
+++ routes/backupRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const backupController = require('../controllers/backupController');
 
-router.get('/admin/api/logiciel/info', backupController.getBackupInfo);
-router.post('/admin/api/logiciel/export', backupController.exportCollections);
+router.get('/info', backupController.getBackupInfo);
+router.post('/export', backupController.exportCollections);
 
 module.exports = router;
