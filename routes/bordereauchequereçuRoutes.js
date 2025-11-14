028d616840fc77b898ec07f3fed8e6741f5db8e0
anwar789456 <anouarsalhi123@gmail.com>
2025-11-14 15:53:21 +0100

    added bank field to userSchema

--- routes/bordereauchequereçuRoutes.js
+++ routes/bordereauchequereçuRoutes.js
@@ -11,18 +11,18 @@ const {
 } = require('../controllers/bordereauchequereçuController');
 
 // Get all bordereauchequereçus
-router.get('/admin/api/logiciel/get-bordereauchequereçus', getAllBordereauchequereçus);
+router.get('/admin/api/logiciel/get-bordereauchequerecus', getAllBordereauchequereçus);
 
 // Get bordereauchequereçu by ID
-router.get('/admin/api/logiciel/get-bordereauchequereçu/:id', getBordereauchequereçuById);
+router.get('/admin/api/logiciel/get-bordereauchequerecu/:id', getBordereauchequereçuById);
 
 // Add new bordereauchequereçu
-router.post('/admin/api/logiciel/add-bordereauchequereçu', addBordereauchequereçu);
+router.post('/admin/api/logiciel/add-bordereauchequerecu', addBordereauchequereçu);
 
 // Update a bordereauchequereçu by _id
-router.put('/admin/api/logiciel/update-bordereauchequereçu/:id', updateBordereauchequereçuById);
+router.put('/admin/api/logiciel/update-bordereauchequerecu/:id', updateBordereauchequereçuById);
 
 // Delete a bordereauchequereçu by _id
-router.delete('/admin/api/logiciel/delete-bordereauchequereçu/:id', deleteBordereauchequereçuById);
+router.delete('/admin/api/logiciel/delete-bordereauchequerecu/:id', deleteBordereauchequereçuById);
 
 module.exports = router;
