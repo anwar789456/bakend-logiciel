d66bcf9fb26955e17b5a24e062577e6a9309cef7
anwar789456 <anouarsalhi123@gmail.com>
2025-11-16 09:41:55 +0100

    updated bon commande fournisseur

--- routes/bonCommandeFournisseurRoutes.js
+++ routes/bonCommandeFournisseurRoutes.js
@@ -10,10 +10,10 @@ const {
 
 // Routes CRUD pour les bons de commande fournisseur
 
-router.post('/admin/api/logiciel/boncommandefournisseur/create-boncommandefournisseur', createBonCommandeFournisseur);
-router.get('/admin/api/logiciel/boncommandefournisseur/get-boncommandefournisseurs', getAllBonCommandeFournisseurs);
-router.get('/admin/api/logiciel/boncommandefournisseur/get-boncommandefournisseur/:id', getBonCommandeFournisseurById);
-router.put('/admin/api/logiciel/boncommandefournisseur/update-boncommandefournisseur/:id', updateBonCommandeFournisseur);
-router.delete('/admin/api/logiciel/boncommandefournisseur/delete-boncommandefournisseur/:id', deleteBonCommandeFournisseur);
+router.post('/admin/api/logiciel/boncommandefournisseur/createboncommandefournisseur', createBonCommandeFournisseur);
+router.get('/admin/api/logiciel/boncommandefournisseur/getboncommandefournisseurs', getAllBonCommandeFournisseurs);
+router.get('/admin/api/logiciel/boncommandefournisseur/getboncommandefournisseur/:id', getBonCommandeFournisseurById);
+router.put('/admin/api/logiciel/boncommandefournisseur/updateboncommandefournisseur/:id', updateBonCommandeFournisseur);
+router.delete('/admin/api/logiciel/boncommandefournisseur/deleteboncommandefournisseur/:id', deleteBonCommandeFournisseur);
 
 module.exports = router;
